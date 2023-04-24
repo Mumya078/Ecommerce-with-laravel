cf66692260be78ffee6c83d760b6e7edc4c9d1ea
Yusuf Emir Tatlı <yusufemirtatli96@gmail.com>
2023-04-24 05:21:27 +0300

    template uploaded and sliced

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index',[HomeController::class,'index'])->name('index');
 
 Route::middleware([
     'auth:sanctum',
